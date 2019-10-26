698b41cef0268e35878b8befd2c38277f90fe29a
AlexApps99 <alex.apps99@gmail.com>
2019-10-26 21:25:35 +1300

    RegEx++, reordered rich presence

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ If you don\'t want to do that find out what to do yourself.
 
 autoinfo = {
   "(pirating|downloading|torrenting|stealing)\s(a\s|an\s|the\s)?(wii\s|gc\s|gamecube\s|game\scube\s)?((game|iso|wad|gcm|rom|wbfs|ciso|img|title)s?\s)?(work|legit|legal|allowed|ok|easy|illegal|dangerous|safe|fast|quick)": piracy,
-  "(to|do|does|can|should|could|give)\s(you\s|i\s|one\s|somebody\s|someone\s|we\s|me\s)?(pirate|download|torrent|steal|free)\s(a\s|an\s|the\s)?(wii\s|gc\s|gamecube\s|game\scube\s)?(game|iso|wad|gcm|rom|wbfs|ciso|img|title)s?": piracy
+  "(to|do|does|can|should|could|give)\s(you\s|i\s|one\s|somebody\s|someone\s|we\s|me\s)?(get|pirate|download|torrent|steal|free)\s(a\s|an\s|the\s)?(.*\s|wii\s|gc\s|gamecube\s|game\scube\s)?(game|iso|wad|gcm|rom|wbfs|ciso|img|title)s?": piracy
 }
 
 if len(argv) == 2:
@@ -42,14 +42,14 @@ bot = commands.Bot(
   case_insensitive=True,
   activity=discord.Activity(
     type=discord.ActivityType.playing,
-    name=choice([
+    name='d!help - '+choice([
       'Super Smash Bros. Melee',
       'Mario Kart Wii',
       'Super Mario Galaxy',
       'Super Mario Sunshine',
       'Xenoblade Chronicles',
       'Wii Party'
-    ])+' - d!help'
+    ])
   )
 )
 
