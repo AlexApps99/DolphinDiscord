e41ca5a17e3cb2dca15b6df50c60645c3ae8044e
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2020-07-06 22:42:02 -0400

    tell Chromebook users to stop using Crostini

--- categories/Info.py
+++ categories/Info.py
@@ -24,6 +24,34 @@ class Info(commands.Cog):
       "Reddit: <https://www.reddit.com/r/DolphinEmulator>\n"
       "Twitter: <https://twitter.com/Dolphin_Emu>"
     ))
-                   
+   
+  @commands.command(help='Gives installation info for Chromebook users', aliases=['chromebook', 'cb'])
+  async def chromebook(self, ctx):
+    '''
+    Gives installation info for Chromebook users
+    '''
+    await ctx.send(embed=discord.Embed(title='Piracy Policy:', description=
+      'Crostini, referred to by Google as "Linux (Beta)," is not suitable for gaming.\n'
+      'This includes Dolphin.\n'
+      "There's a couple reasons for this, including (at the time this was written):\n"
+      '-Broken OpenGL drivers (no ARB_buffer_storage, something Dolphin uses for performance)\n'
+      "-No Vulkan support (which runs better than OpenGL in just about every instance where it's supported)\n"
+      '-The whole thing runs in a VM instead of having any sort of direct hardware access\n'
+      '-Absolutely no controller support over Bluetooth or USB whatsoever\n'
+      'If none of that means anything to you, in English that basically means Dolphin will run very badly if you use the ancient stable versions,\n'
+      "and it won't run at all if you use a beta or development version.\n"
+      'If you want to run Dolphin on your Chromebook, you have a few options.\n'
+      'These options are out of the scope of what we can provide support for here, unfortunately\n'
+      "Your options are (do some research on these!):\n"
+      '-Crouton (run a Linux operating system alongside Chrome OS and use a keyboard shortcut to switch between them at will)\n'
+      '-chrx (dual boot Chrome OS and a Linux operating system - GalliumOS is HIGHLY recommended for Chromebooks)\n'
+      '-Google is apparently working on some sort of new Ubuntu environment for Chromebooks called Borealis that has Steam preinstalled, so you could try setting up Dolphin on that whenever it comes out\n'
+      "You'll have to find other support forums for these installations and be a bit computer savvy,\n"
+      'but when you get them set up then this server can help you get Dolphin installed very easily.\n'
+
+    ))
+    
+    
+    
 def setup(bot):
   bot.add_cog(Info(bot))
