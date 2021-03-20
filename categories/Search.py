603f556384467d910f823d4c9ecfed2da6804cb1
John Papetti <jfp3713@gmail.com>
2021-03-20 16:48:25 -0400

    Update Search.py

--- categories/Search.py
+++ categories/Search.py
@@ -240,7 +240,7 @@ class Search(commands.Cog):
         )
 
   @commands.command(
-    help='Search for Netplay sessions on the index, by name or RoomID (traversal only).',
+    help='Search for Netplay sessions on the index.',
     aliases=['np','party']
   )
   async def netplay(self, ctx, *, query: str):
