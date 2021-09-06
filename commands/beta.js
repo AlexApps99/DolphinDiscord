921bb9ca5c0d50e19ed6ce99f217e9b6fbf6454e
AlexApps99 <alex.apps99@gmail.com>
2021-09-06 16:10:48 +1200

    Added beta and latest commands

---
+++ commands/beta.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
+
+function get_update_info() {
+	return fetch("https://dolphin-emu.org/update/check/v1/beta/31524288e3b2450eaefff8202c6d26c4ba3f7333/win").then(res => {
+		if (res.ok) {
+			return res.json();
+		} else {
+			throw new Error(`Invalid status: ${res.status} ${res.statusText}`);
+		}
+	});
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('beta')
+		.setDescription('Get the version of the latest beta release of Dolphin'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		let i = await get_update_info();
+		console.log(i);
+		return interaction.editReply(i.new.name + " (" + i.new.hash + ")");
+	},
+};
