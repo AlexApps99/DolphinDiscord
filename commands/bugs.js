2c119692f81a2414091cf6d62b85abc00b42140c
AlexApps99 <alex.apps99@gmail.com>
2021-09-06 20:45:18 +1200

    Netplay 2/2

--- commands/bugs.js
+++ commands/bugs.js
@@ -3,6 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
 
 const INT_REG = /^\d+$/;
+const LIMIT = 3;
 
 function search_by_id(id) {
 	return fetch("https://bugs.dolphin-emu.org/issues/" + encodeURIComponent(id.toString()) + ".json").then(res => {
@@ -15,7 +16,7 @@ function search_by_id(id) {
 }
 
 function search_by_query(query) {
-	return fetch("https://bugs.dolphin-emu.org/projects/emulator/search.json?limit=3&all_words=1&open_issues=1&titles_only=&q=" + encodeURIComponent(query)).then(res => {
+	return fetch("https://bugs.dolphin-emu.org/projects/emulator/search.json?limit=" + LIMIT.toString() + "&all_words=1&open_issues=1&titles_only=&q=" + encodeURIComponent(query)).then(res => {
 		if (res.ok) {
 			return res.json();
 		} else {
@@ -25,7 +26,7 @@ function search_by_query(query) {
 }
 
 function isInt(val) {
-  return INT_REG.test(val);
+	return INT_REG.test(val);
 }
 
 module.exports = {
@@ -56,110 +57,26 @@ module.exports = {
 			if (fixed) embed = embed.addField("Fixed in:", fixed.toString() + ' | http://dolp.in/v' + fixed.toString());
 			return interaction.editReply({ content: "**Bug report for:** ***" + query + "***", embeds: [embed] });
 		} else {
-			return interaction.editReply("Sorry, this command has not been implemented yet. Ping <@292383975048216576> until he finishes rewriting the bot.");
+			const r = await search_by_query(query);
+			const hits = r.total_count;
+			let max = hits;
+			if (!r.total_count) {
+				return interaction.editReply("Sorry, your search could not be found.");
+			} else if (r.total_count > LIMIT) {
+				max = LIMIT;
+			}
+			let embed = new MessageEmbed()
+				.setTitle('**Bug Search Results**')
+				.setURL("https://bugs.dolphin-emu.org/projects/emulatio/search?limit=25&all_words=1&open_issues=1&titles_only=&q=" + encodeURIComponent(query))
+				.setColor(0x3FCAFF)
+				.setAuthor('Dolphin Bug Reporter', 'https://wiki.dolphin-emu.org/images/dolphin.40.png', 'https://bugs.dolphin-emu.org/projects/emulator')
+				.setFooter('Total results: ' + hits.toString(), 'https://wiki.dolphin-emu.org/images/dolphin.40.png');
+			for (let i = 0; i < max; i++) {
+				const date = Date.parse(r.results[i].datetime);
+				const url = r.results[i].title;
+				embed = embed.addField(r.results[i].title, `Date Reported: ${date} | ${url}`);
+			}
+			return interaction.editReply({ content: "**Search result for:** ***" + query + "***:", embeds: [embed] });
 		}
 	},
 };
-
-/*
-  @commands.command(
-    help='Search for bugs with Dolphin Bug Reporter',
-    aliases=['b']
-  )
-  async def bugs(self, ctx, *, query: str):
-    '''
-    Uses Dolphin Redmine to lookup bug reports
-    '''
-    if (query.isdecimal()):
-        try:
-            sea = requests.get(
-              f'https://bugs.dolphin-emu.org/issues/{query}.json'
-            ).json()['issue']
-        except ValueError:
-            await ctx.send('Sorry, your search could not be found.')
-            return
-
-        id = sea['id']
-        status = sea['status']['name']
-        author = sea['author']['name']
-        subject = sea['subject']
-        fixed = sea['custom_fields'][9]['value']
-        update = datetime.datetime.strptime(sea['updated_on'],"%Y-%m-%dT%H:%M:%SZ")
-
-        embed = discord.Embed(
-            title=f'**{subject}**',
-            url=f'https://bugs.dolphin-emu.org/issues/{id}',
-            color=0xFC2C03
-        )
-        embed.set_footer(
-          text='Report last modified',
-          icon_url='https://wiki.dolphin-emu.org/images/dolphin.40.png'
-        )
-        embed.set_author(
-          name='Dolphin Bug Reporter',
-          url='https://bugs.dolphin-emu.org/projects/emulator',
-          icon_url='https://wiki.dolphin-emu.org/images/dolphin.40.png'
-        )
-        embed.timestamp=update
-        embed.add_field(
-          name="Reported by:",
-          value=author,
-          inline=False
-        )
-        embed.add_field(
-          name="Status:",
-          value=status,
-          inline=False
-        )
-        if (fixed):
-          embed.add_field(
-            name="Fixed in:",
-            value=f'{fixed} | http://dolp.in/v{fixed}',
-            inline=False
-          )
-        await ctx.send(
-          f'**Bug report for:** ***{query}***:',
-          embed=embed
-        )
-
-    else:
-        sea=requests.get(
-            'https://bugs.dolphin-emu.org/projects/emulator/search.json?'
-            f'limit=3&all_words=1&open_issues=1&titles_only=&q="{query}"'
-        ).json()
-        hits = sea['total_count']
-        if not hits:
-            await ctx.send('Sorry, your search could not be found.')
-            return
-        if (hits > 3):
-            max = sea['limit']
-        else:
-            max = sea['total_count']
-        embed = discord.Embed(
-            title=f'**Bug Search Results**',
-            url='https://bugs.dolphin-emu.org/projects/emulator/search?'
-            f'limit=25&all_words=1&open_issues=1&titles_only=&q="{urllib.parse.quote_plus(query)}"',
-            color=0xFC2C03
-        )
-        embed.set_author(
-          name='Dolphin Bug Reporter',
-          url='https://bugs.dolphin-emu.org/projects/emulator',
-          icon_url='https://wiki.dolphin-emu.org/images/dolphin.40.png'
-        )
-        embed.set_footer(
-          text=f'Total results: {hits}',
-          icon_url='https://wiki.dolphin-emu.org/images/dolphin.40.png'
-        )
-        for x in range(max):
-           date = datetime.datetime.strptime(sea['results'][x]['datetime'],"%Y-%m-%dT%H:%M:%SZ")
-           url = sea['results'][x]['url']
-           embed.add_field(
-              name=sea['results'][x]['title'],
-              value=f'Date Reported: {date} | {url}',
-              inline=False
-            )
-        await ctx.send(
-          f'**Search result for:** ***"{query}"***:',
-          embed=embed
-        )
-*/
