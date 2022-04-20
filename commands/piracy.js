62fbaf115b766ac75581a6beebe8340d4fdd1648
memequester <memequester@gmail.com>
2022-04-21 00:17:21 +0100

    BetterJoy and ryo DS4Windows commands
    Also updated the piracy text to better reflect the anti-piracy stance of the server

--- commands/piracy.js
+++ commands/piracy.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const text = `Piracy is not allowed in this server. Please read <#521711544925552642>!
-We will only support the dumping of games you own.
-If you don't want to do that, find out what to do yourself.`;
+const text = `Piracy is illegal, and is not tolerated in this server. Previously owning a game does not make downloading the game legal. Please read <#521711544925552642>!
+We will only provide support for games that you have dumped.
+Dump your games, or find out what to do yourself.`;
 
 const embed = new MessageEmbed()
 	.setTitle('Piracy Policy:')
